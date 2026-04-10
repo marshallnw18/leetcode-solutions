fbebe3ab89183d29db967d141b15e7868de87a5e
Nick Marshall <32916930+marshallnw18@users.noreply.github.com>
2026-04-09 20:38:29 -0400

    Time: 75 ms (58.81%) | Memory: 34 MB (33.94%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,12 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+
+        for i in asteroids:
+            if i > mass:
+                return False
+            else:
+                mass += i
+        
+        return True 
+
